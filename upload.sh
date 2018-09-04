3da3442354164b06e44921efba372f988adce566
Rinoahu <xiaohu@iastate.edu>
2018-09-04 17:16:43 -0500

    remove test.txt file

---
+++ upload.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#cd MCL_lite
+git config --global user.email xiaohu@iastate.edu
+git config --global user.name Rinoahu
+
+
+git remote rm origin
+
+git add -A .
+git commit -m 'remove test.txt file'
+git remote add origin https://github.com/Rinoahu/MCL_lite
+
+git pull origin master
+git push origin master
+
+git checkout master
