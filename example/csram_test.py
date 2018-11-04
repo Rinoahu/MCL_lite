07d12c7331485794075fe24b6d1cfe5e5e76bed6
Rinoahu <xiaohu@iastate.edu>
2018-11-03 21:32:30 -0500

    improve memory usage of csrmm function

--- example/csram_test.py
+++ example/csram_test.py
@@ -1,3 +1,8 @@
+#!usr/bin/env python
+
+import sys
+sys.path.append('../')
+
 import mcl_sparse
 
 x = mcl_sparse.sparse.random(10000, 10000, 1e-3, format='csr')
@@ -8,5 +13,8 @@ z = mcl_sparse.csram_ez_ms(x, y)
 print x.indptr
 print y.indptr
 
-yp = mcl_sparse.csram_p_ez(x, y, cpu=3)
+zp = mcl_sparse.csram_p_ez(x, y, cpu=3)
 
+
+dif = zp - z
+print dif.nnz
