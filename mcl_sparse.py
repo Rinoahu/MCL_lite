55cf3da6f8b864c2c801571a4458d93296cd98d2
Rinoahu <xiaohu@iastate.edu>
2018-12-16 22:10:08 -0600

    reduce disk space usage

--- mcl_sparse.py
+++ mcl_sparse.py
@@ -18210,10 +18210,13 @@ def expand_t(xyz):
 
 def expand_prune_inflate_t(xyz):
     fnx, fns, I, prune, pct, R, S, inplace, cpu, mem, tmp_path = xyz
+
+    #print 'fnx, fns', fnx, fns
+
+    # expansion
     x = load_npz_disk(fnx)
     fnz = fnx + '_z.npy'
     # update x
-
     fny = fns[0]
     y = load_npz_disk(fny)
     z = csrmm_p_ez_fast(y, x, prefix=fnz, cpu=cpu, disk=True)
@@ -18230,6 +18233,7 @@ def expand_prune_inflate_t(xyz):
     mi, ct = prune_p_ez(x, prune=prune, pct=pct, R=R, S=S, cpu=cpu, inplace=inplace, mem=mem)
     csr_close(x)
 
+    # eliminate
     x = load_npz_disk(fnx)
     y = sparse.csr_matrix(x.shape)
     z = csram_p_ez(x, y, prefix=fnx+'_elm.npy', tmp_path=tmp_path, disk=True, cpu=cpu)
@@ -18259,7 +18263,7 @@ def expand_prune_inflate_disk(qry, shape=(10**8, 10**8), tmp_path=None, I=1.5, c
     if not fnmerge:
         fnmerge = fns
 
-    chaos = map(expand_prune_inflate_t, [[fnx, fns, I, prune, pct, R, S, inplace, cpu, mem, tmp_path] for fnx in fns])
+    chaos = map(expand_prune_inflate_t, [[fnx, fnmerge, I, prune, pct, R, S, inplace, cpu, mem, tmp_path] for fnx in fns])
 
     return max(chaos)
 
@@ -18342,6 +18346,7 @@ def mcl_nr_disk(qry, tmp_path=None, xy=[], I=1.5, prune=1/4e3, select=1100, reco
 
         #chao = expand_prune_inflate_disk(qry, shape=shape, tmp_path=tmp_path, cpu=cpu, mem=mem)
         chao = expand_prune_inflate_disk(qry, shape=shape, tmp_path=tmp_path, I=I, cpu=cpu, mem=mem, prune=prune, pct=pct, R=recover, S=select, inplace=1)
+        print 'chao is', chao, chao_old
 
         # remove Mg file
         if alg == 'mcl':
@@ -18360,7 +18365,6 @@ def mcl_nr_disk(qry, tmp_path=None, xy=[], I=1.5, prune=1/4e3, select=1100, reco
             nochange = 0
 
         chao_old = chao
-
 
         if chao < 1e-3 and it > 0:
             break
